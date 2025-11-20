019ea3be153f40603b4aabfb37fb3409421b164a
Anton Liashenka <a.y.lyashenko@gmail.com>
2025-11-20 11:31:34 +0300

    refactoring

--- server/src/main/java/tech/amak/portbuddy/server/web/AuthController.java
+++ server/src/main/java/tech/amak/portbuddy/server/web/AuthController.java
@@ -2,13 +2,13 @@ package tech.amak.portbuddy.server.web;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.http.HttpStatus;
 
 import lombok.RequiredArgsConstructor;
 import tech.amak.portbuddy.server.security.JwtService;
