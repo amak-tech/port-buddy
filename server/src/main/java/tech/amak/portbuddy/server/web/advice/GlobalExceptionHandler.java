5bde2a1b9fdad8f6589ff71bbd39d51f09d6412e
Anton Liashenka <a.y.lyashenko@gmail.com>
2025-12-02 16:52:13 +0300

    add logging

--- server/src/main/java/tech/amak/portbuddy/server/web/advice/GlobalExceptionHandler.java
+++ server/src/main/java/tech/amak/portbuddy/server/web/advice/GlobalExceptionHandler.java
@@ -11,16 +11,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResponseStatusException.class)
     public ProblemDetail handleResponseStatusException(final ResponseStatusException ex) {
+        log.error(ex.getMessage(), ex);
         return ProblemDetail.forStatusAndDetail(ex.getStatusCode(), ex.getReason());
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleGlobalException(final Exception ex) {
+        log.error(ex.getMessage(), ex);
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 }
