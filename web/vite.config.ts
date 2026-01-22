3c85d51e5ff981147190640a4bf4e3cd654103f6
Anton Liashenka <a.y.lyashenko@gmail.com>
2026-01-22 10:58:40 +0300

    update config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('development')
+  },
   plugins: [
     react(),
     Prerender({
@@ -16,4 +19,8 @@ export default defineConfig({
       staticDir: path.join(__dirname, 'dist'),
     }),
   ],
+  build: {
+    minify: false,
+    sourcemap: true,
+  },
 })
